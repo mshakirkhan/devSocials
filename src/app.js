ec4d96d639439389217c5b750b17e9fc12b9b205
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2024-09-25 01:23:03 +0500

    Feat: User CRUD

--- src/app.js
+++ src/app.js
@@ -3,16 +3,45 @@ require('./config/database')
 const app = express();
 const User = require('./models/user')
 
-app.post("/signup", (req, res) => {
-    const user = new User({
-        firstName: "Muhammad Shakir",
-        lastName: "Khan",
-        email: "shakir.sedoc@gmail.com",
-        age: 28,
-        gender: "male"
-    })
-    user.save();
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const data = req.body;
+    const user = new User(data)
+    await user.save();
     res.send("User added successfully!")
+})
+
+app.get("/listUser", async (req, res) => {
+    try {
+        const user = await User.find({});
+        res.send(user);
+    }
+    catch(err) {
+        res.status(500).send(err.message);
+    }
+})
+
+app.delete("/deleteUser", async(req, res) => {
+    const {id} = req.body;
+    try {
+        await User.findByIdAndDelete(id);
+        res.send("User deleted successfully");
+    }
+    catch(err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.patch("/updateUser", async (req, res) => {
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate(data.id, data, {returnDocument:'before'});
+        res.send(user);
+    }
+    catch(err) {
+        res.status(500).send(err.message);
+    }
 })
 
 app.listen(2000, (req, res) => {
